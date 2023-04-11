5a677db3dfffbcf1c8a83ef1953bd0633fd2c761
Dubi253 <dubi253@outlook.com>
2023-04-11 16:12:59 +0100

    feat: add vuepress export pdf

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'en-US',
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: 'Low-Code Data Analysis',
+  description: 'A simplified approach to data analysis with less coding required.',
   base: '/Demo-Documentation/' // docs: https://v2.vuepress.vuejs.org/guide/deployment.html#github-pages
 })
